0507b233a8cfcfc8d21d540872d9c6e94c0e35d2
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2018-10-22 22:09:28 -0300

    Correção da chamada

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -5,18 +5,18 @@ const { respondSuccess, respondErr } = require('../utils/responseUtils')
 const { messageService } = require('../services')
 
 const BOOT_ID = '5bce2c664d5eae61d4bb6a59'
-
+/*
 const agent = tunnel.httpsOverHttp({
     proxy: {
         host: '127.0.0.1',
         port: 3128,
     },
 })
-
+*/
 const axiosClient = axios.create({
     baseURL: 'https://api.dialogflow.com/v1/query?v=20150910:443',
-    httpsAgent: agent,
-    proxy: false,
+    //httpsAgent: agent,
+    //proxy: false,
 })
 
 const router = express.Router()
@@ -51,21 +51,26 @@ router.post('/chatbot', async (req, res) => {
     let baseURL = '/'
     let token = '248f1c49d93b4c2c9114d3259e888817'
 
+    let data = req.body.data || {}
+    let userId = req.body.userId || ''
+    let tempId = req.body.tempId || ''
+
     const userMessage = await messageService.insert({
-        userId: req.body.userId,
-        sessionId: req.body.context.sessionId,
-        text: req.body.context.query,
+        userId: userId,
+        sessionId: data.sessionId,
+        text: data.query,
         createdAt: new Date(),
     })
 
-    axiosClient.post(baseURL, req.body.context, { headers: { Authorization: `Bearer ${token}` } }).then(async response => {
+    axiosClient.post(baseURL, data, { headers: { Authorization: `Bearer ${token}` } }).then(async response => {
+        console.log('chegou aqui')
         const systemMessage = await messageService.insert({
             userId: BOOT_ID,
             sessionId: response.data.sessionId,
             text: response.data.result.fulfillment.messages[0].speech,
             createdAt: new Date(),
         })
-        respondSuccess(res, 200, { context: response.data, messages: [systemMessage, userMessage] })
+        respondSuccess(res, 200, { context: response.data, userMessages: { tempId, _id: userMessage._id }, systemMessage })
     }).catch(err => respondErr(res, 500, err))
 })
 
